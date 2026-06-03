2738b20ce0b63e0add562768149fd5e68417fccc
Saddam <eyarinsultana@gmail.com>
2026-06-03 08:18:59 +0600

    proxy file change

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,21 +1,36 @@
 import dns from "node:dns";
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+// import { auth } from "./lib/auth";
+// import { headers } from "next/headers";
+
+// export async function proxy(request: NextRequest) {
+//   const session = await auth.api.getSession({
+//     headers: await headers(),
+//   });
+//   if (session) {
+//     return NextResponse.next();
+//   }
+//   return NextResponse.redirect(new URL("/login", request.url));
+// }
+
+// export const config = {
+//   matcher: ["/admin"],
+// };
+
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { auth } from "./lib/auth";
-import { headers } from "next/headers";
 
-export async function proxy(request: NextRequest) {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
-  if (session) {
-    return NextResponse.next();
-  }
-  return NextResponse.redirect(new URL("/login", request.url));
+// This function can be marked `async` if using `await` inside
+export function proxy(request: NextRequest) {
+  return NextResponse.redirect(new URL("/home", request.url));
 }
 
+// Alternatively, you can use a default export:
+// export default function proxy(request: NextRequest) { ... }
+
 export const config = {
-  matcher: ["/admin"],
+  matcher: "/about/:path*",
 };
